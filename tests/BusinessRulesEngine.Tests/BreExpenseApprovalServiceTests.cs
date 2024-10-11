19e6529f92eec4c50f811468083fa7d611608e61
pacodelacruz <pacodelacruz@users.noreply.github.com>
2024-10-11 22:55:15 +1100

    Cleaned up test assertions

--- tests/BusinessRulesEngine.Tests/BreExpenseApprovalServiceTests.cs
+++ tests/BusinessRulesEngine.Tests/BreExpenseApprovalServiceTests.cs
@@ -43,19 +43,19 @@ namespace BusinessRulesEngine.Tests
         }
 
         [Theory]
-        [InlineData("Meal-Manager-Under50.json", "Approved", false)]
-        [InlineData("Meal-Manager-Above50.json", "Rejected", true)]
-        [InlineData("Meal-Manager-Under50-Weekend.json", "RequiresManualApproval", true)]
-        [InlineData("Meal-Boss-Above50.json", "Approved", false)]
-        [InlineData("Meal-Boss-Above1000.json", "Rejected", true)]
-        [InlineData("Flight-Manager-MEL-SYD-1000.json", "Approved", false)]
-        [InlineData("Flight-Manager-MEL-PER-1600.json", "Rejected", true)]
-        [InlineData("Flight-Manager-SYD-AKL-3000.json", "RequiresManualApproval", true)]
-        [InlineData("Flight-Boss-HBA-CBR-2500.json", "Approved", false)]
-        [InlineData("Flight-Boss-HBA-CBR-2501.json", "RequiresManualApproval", true)]
-        [InlineData("Flight-Boss-BNE-LAX-3499.json", "Approved", false)]
-        [InlineData("Flight-Boss-BNE-DFW-3501.json", "RequiresManualApproval", true)]
-        public async Task TestExpenses(string payloadFileName, string expectedStatus, bool requiresStatusReason = false)
+        [InlineData("Meal-Manager-Under50.json", "Approved")]
+        [InlineData("Meal-Manager-Above50.json", "Rejected")]
+        [InlineData("Meal-Manager-Under50-Weekend.json", "RequiresManualApproval")]
+        [InlineData("Meal-Boss-Above50.json", "Approved")]
+        [InlineData("Meal-Boss-Above1000.json", "Rejected")]
+        [InlineData("Flight-Manager-MEL-SYD-1000.json", "Approved")]
+        [InlineData("Flight-Manager-MEL-PER-1600.json", "Rejected")]
+        [InlineData("Flight-Manager-SYD-AKL-3000.json", "RequiresManualApproval")]
+        [InlineData("Flight-Boss-HBA-CBR-2500.json", "Approved")]
+        [InlineData("Flight-Boss-HBA-CBR-2501.json", "RequiresManualApproval")]
+        [InlineData("Flight-Boss-BNE-LAX-3499.json", "Approved")]
+        [InlineData("Flight-Boss-BNE-DFW-3501.json", "RequiresManualApproval")]
+        public async Task TestExpenses(string payloadFileName, string expectedStatus)
         {
             _consoleLogger.Log(LogLevel.Information, $"Testing PayloadFileName: {payloadFileName}");
 
@@ -76,10 +76,6 @@ namespace BusinessRulesEngine.Tests
             Assert.Equal(expectedExpenseId, processExpressClaimResult.ExpenseId?.ToString());
             // Is the status as expected?
             Assert.Equal(expectedStatus, processExpressClaimResult.Status?.ToString());
-            if (requiresStatusReason)
-            {
-                Assert.NotNull(processExpressClaimResult.StatusReason);
-            }
         }
     }
 }
